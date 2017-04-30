291c804d6875778eb4265fd532b6fc45ba1d6c5f
pfiuk <pawel.fiuk@upaid.pl>
2017-04-30 21:19:08 +0200

    Added theme generation script Deleted dotfiles that I am no longer using

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -12,32 +12,22 @@
         set __fish_git_prompt_char_upstream_ahead '↑'
         set __fish_git_prompt_char_upstream_behind '↓'
 
-# Prompt color definitions
-	set COLOR_USERNAME "A61"
-	set COLOR_MONKEY "444"
-	set COLOR_HOSTNAME "AF2"
-	set COLOR_PATH "F72"
-	set COLOR_ARROW_1 "F82"
-	set COLOR_ARROW_2 "D61"
-	set COLOR_ARROW_3 "B40"
-	set COLOR_GIT "F82"
-
-# Prompt character
+# Prompt character ❯
 	set PROMPT_CHAR "❯"
 
 function fish_prompt
 	# Username
 
-	printf (set_color -o $COLOR_USERNAME)(whoami | head -c 1)' '
+	printf (set_color -o $GENERATED_PALETTE_8)(whoami | head -c 1)' '
 
 	# Current path
 
-	echo -n (set_color normal)(set_color $COLOR_PATH)(prompt_pwd)
+	echo -n (set_color normal)(set_color $GENERATED_PALETTE_7)(prompt_pwd)
 
 	# GIT status
 
 	set last_status $status
-	set_color -o $COLOR_GIT
+	set_color -o $GENERATED_PALETTE_6
 	if __fish_git_prompt
 		printf '\b'
 	else
@@ -46,8 +36,8 @@ function fish_prompt
 
 	# Fancy arrows
 
-	echo -n (set_color -o $COLOR_ARROW_1)"$PROMPT_CHAR"
-	echo -n (set_color -o $COLOR_ARROW_2)"$PROMPT_CHAR"
-	echo -n (set_color -o $COLOR_ARROW_3)"$PROMPT_CHAR "
+	echo -n (set_color -o $GENERATED_PALETTE_6)"$PROMPT_CHAR"
+	echo -n (set_color -o $GENERATED_PALETTE_5)"$PROMPT_CHAR"
+	echo -n (set_color -o $GENERATED_PALETTE_4)"$PROMPT_CHAR "
 	set_color normal
 end
